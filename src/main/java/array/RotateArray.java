fbcd73c3ea1f7fafb162221e547977d80f6deb69
tianyu <jiatianyujay@gmail.com>
2025-02-19 23:10:47 +0800

    旋转数组

---
+++ src/main/java/array/RotateArray.java
@@ -0,0 +1,39 @@
+package array;
+
+import java.util.Arrays;
+
+/**
+ * leetcode189 轮转数组
+ * 给定一个整数数组 nums，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
+ * 输入: nums = [1,2,3,4,5,6,7], k = 3
+ * 输出: [5,6,7,1,2,3,4]
+ * 解释:
+ * 向右轮转 1 步: [7,1,2,3,4,5,6]
+ * 向右轮转 2 步: [6,7,1,2,3,4,5]
+ * 向右轮转 3 步: [5,6,7,1,2,3,4]
+ */
+public class RotateArray {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,2,3,4,5,6,7};
+        rotate(nums, 3);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void rotate(int[] nums, int k){
+        k %= nums.length;
+        reverse(nums,0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public static void reverse(int[] nums, int left, int right){
+        while (left < right){
+            int tmp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = tmp;
+            left++;
+            right--;
+        }
+    }
+
+}
